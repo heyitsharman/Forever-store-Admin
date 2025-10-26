77abacdd7352866ce6f055c9f854294c91c6a210
heyitsharman <harmankaur0779@gmail.com>
2025-10-26 09:46:34 +0530

    added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-
+import Sidebar from './components/Sidebar'
 const App = () => {
   return (
-    <>
+    <div className='bg-gray-50 min-h-screen'>
+      <>
       <Navbar></Navbar>
-    </>
+      <hr />
+      <div className='flex w-full'>
+        <Sidebar></Sidebar>
+      </div>
+      </>
+      
+
+    </div>
   )
 }
 
