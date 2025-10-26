3fec24401dc6a65d35b9370cd9be114e28796b5d
heyitsharman <harmankaur0779@gmail.com>
2025-10-26 09:57:08 +0530

    React router dom to create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+
+import {Routes, Route} from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
 const App = () => {
+
+
+
+
   return (
     <div className='bg-gray-50 min-h-screen'>
       <>
@@ -9,6 +18,13 @@ const App = () => {
       <hr />
       <div className='flex w-full'>
         <Sidebar></Sidebar>
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+          <Routes>
+            <Route path='/add' element ={<Add />}/>
+            <Route path='/list' element ={<List />}/>
+            <Route path='/orders' element ={<Orders/>}/>
+          </Routes>
+        </div>
       </div>
       </>
       
